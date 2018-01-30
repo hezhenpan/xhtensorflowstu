0b744bc2aa8ee82798205237b02c6de2fa718648
hezhenpan <hezhenpan@163.com>
2018-01-30 19:55:56 +0800

    添加一维线性方程
    Signed-off-by: hezhenpan <hezhenpan@163.com>

--- tftest.py
+++ tftest.py
@@ -1,6 +1,62 @@
 # *_*coding:utf-8 *_*
 
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import xlrd
+
+DATA_FILE = "./data/fire_theft.xls"
+
+book = xlrd.open_workbook(DATA_FILE, encoding_override="utf-8")
+sheet = book.sheet_by_index(0)
+data = np.asarray([sheet.row_values(i) for i in range(1, sheet.nrows)])
+n_samples = sheet.nrows - 1
+
+X = tf.placeholder(tf.float32, name="X")
+Y = tf.placeholder(tf.float32, name="Y")
+
+w = tf.Variable(0.0, name="weights")
+b = tf.Variable(0.0, name="bais")
+
+Y_predicted = X * w + b
+
+loss = tf.square(Y - Y_predicted, name="loss")
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for i in range(100):
+        for x, y in data:
+            sess.run([w, b])
+
+X, Y = data.T[0], data.T[1]
+
+plt.plot(X, Y, 'bo', label='Real data')
+plt.plot(X, X * w + b, 'r', label='Predicted data')
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # a = tf.constant(2, name="a")
 #
@@ -47,8 +103,8 @@ import tensorflow as tf
 # sess1.close()
 # sess2.close()
 
-a = tf.placeholder(tf.float32, shape=[3])
-
-b = tf.constant([5, 5, 5], tf.float32)
-
-c = a + b
+# a = tf.placeholder(tf.float32, shape=[3])
+#
+# b = tf.constant([5, 5, 5], tf.float32)
+#
+# c = a + b
