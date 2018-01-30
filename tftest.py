4977d347316bc3db76c4b50659d38b3591d4290f
hezhenpan <hezhenpan@163.com>
2018-01-30 10:52:17 +0800

    添加文件

--- tftest.py
+++ tftest.py
@@ -2,17 +2,39 @@
 
 import tensorflow as tf
 
-a = tf.constant(2,name="a")
+# a = tf.constant(2, name="a")
+#
+# b = tf.constant(3, name="b")
+#
+# x = tf.add(a, b, name="add")
+#
+# r = tf.random_normal([2, 3], mean=1.0, dtype=tf.float32, name="random")
+#
+# with tf.Session() as sess:
+#     writer = tf.summary.FileWriter('./graphs', sess.graph)
+#     print(sess.run(r))
 
-b = tf.constant(3,name="b")
 
-x = tf.add(a,b,name="add")
+# my_const = tf.constant([1.0, 2.0], name="my_const")
 
-r = tf.random_normal([2,3],mean=1.0,dtype=tf.float32,name="random")
+# print(tf.get_default_graph().as_graph_def())
+
+# a = tf.Variable(2, name="scalar")
+#
+# b = tf.Variable([2, 3], name="vector")
+#
+# c = tf.Variable([[0, 1], [2, 3]], "matrix")
+#
+# w = tf.Variable(tf.zeros([784, 10]))
+#
+# init = tf.global_variables_initializer()
+
+w = tf.Variable(tf.truncated_normal([700,10]))
+
 
 with tf.Session() as sess:
-    writer = tf.summary.FileWriter('./graphs',sess.graph)
-    print(sess.run(r))
+    sess.run(w.initializer)
+    print(w.eval())
 
 
 
