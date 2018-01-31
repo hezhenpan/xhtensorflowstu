a9a7034dd3af7d4e4e82a8a2594e0fa0771f5e4e
hezhenpan <hezhenpan@163.com>
2018-01-31 19:57:54 +0800

    试试绘图1
    Signed-off-by: hezhenpan <hezhenpan@163.com>

--- huitu.py
+++ huitu.py
@@ -1,7 +1,9 @@
 # *_*coding:utf-8 *_*
 
+import numpy as np
 import matplotlib.pyplot as plt
 
-plt.figure(1)
-plt.figure(2)
+plt.figure(1, dpi=50)
+x = np.linspace(-np.pi, np.pi, 100)
+plt.plot(x, np.sin(x))
 plt.show()
